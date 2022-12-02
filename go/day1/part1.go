bc8a880fa8e97a57e3423ec62392ce5c4a6ee374
nrr <nickrrau@gmail.com>
2022-12-01 20:26:22 -0500

    Part one solution in Go
    Was initially going for more complex but decided to opt for simple
    and concise.
    
    Results of input:
    λ .\part1.exe p1_example.input
    AoC Day 1 Part 1
    24000
    
    λ .\part1.exe p1_sample.input
    AoC Day 1 Part 1
    69310

--- go/day1/part1.go
+++ go/day1/part1.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("AoC Day 1 Part 1")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Need to provide input")
+		return
+	}
+
+	input := os.Args[1]
+	processInput(input)
+}
+
+func processInput(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	highest_cal := 0
+	tmp_cal := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if len(scanner.Text()) == 0 {
+			tmp_cal = 0
+			continue
+		}
+
+		cal, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+
+		tmp_cal = tmp_cal + cal
+
+		if highest_cal < tmp_cal {
+			highest_cal = tmp_cal
+		}
+	}
+	fmt.Println(highest_cal)
 }
