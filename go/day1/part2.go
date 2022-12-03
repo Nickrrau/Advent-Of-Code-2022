6acd8d75da46dc15fe52b3c2263289ddd189dca3
nrr <nickrrau@gmail.com>
2022-12-03 03:51:58 -0500

    Part 2 solution for Day 1 in Go
    Not happy with the way pushing onto the array works but
    still only scans over the file once.
    
    λ .\part2.exe p1_example.input
    AoC Day 1 Part 2
    45000
    λ .\part2.exe p1_sample.input
    AoC Day 1 Part 2
    206104)

--- go/day1/part2.go
+++ go/day1/part2.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("AoC Day 1 Part 2")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Need to provide input")
+		return
+	}
+
+	input := os.Args[1]
+	processInput(input)
+}
+
+func processInput(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	top_three_cals := [3]int{0, 0, 0}
+	tmp_cal := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		if len(input) < 1 {
+			pushPop(&top_three_cals, tmp_cal)
+			tmp_cal = 0
+		} else {
+			cal, err := strconv.Atoi(input)
+			if err != nil {
+				break
+			}
+			tmp_cal = tmp_cal + cal
+			continue
+		}
+	}
+	pushPop(&top_three_cals, tmp_cal)
+
+	fmt.Println(top_three_cals[0] + top_three_cals[1] + top_three_cals[2])
+}
+
+func pushPop(stack *[3]int, value int) {
+	if stack[0] < value {
+		stack[2] = stack[1]
+		stack[1] = stack[0]
+		stack[0] = value
+	} else if stack[1] < value {
+		stack[2] = stack[1]
+		stack[1] = value
+	} else if stack[2] < value {
+		stack[2] = value
+	}
 }
