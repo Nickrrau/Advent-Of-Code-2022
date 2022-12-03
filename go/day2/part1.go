021042e1296548e9ff1f7c17ad42aca2a5158db7
nrr <nickrrau@gmail.com>
2022-12-03 04:08:48 -0500

    Setup for Day 2, in Go

---
+++ go/day2/part1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("AoC Day 2 Part 1")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Need to provide input")
+		return
+	}
+
+	input := os.Args[1]
+	processInput(input)
+}
+
+func processInput(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	}
+}
