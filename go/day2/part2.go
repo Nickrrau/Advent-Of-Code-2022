9616a591f4a9b34b392c1543e201816bfe4095ce
nrr <nickrrau@gmail.com>
2022-12-06 21:24:12 -0500

    Day 2 Part 2 Solution in Go
    Similar vain to part one, verbose but somewhat readable, there is probably a more
    elegant solution I'm missing here but the result works out.
    
    Example:
    AoC Day 2 Part 2
    12
    Sample:
    AoC Day 2 Part 2
    13448

--- go/day2/part2.go
+++ go/day2/part2.go
@@ -18,6 +18,20 @@ func main() {
 	processInput(input)
 }
 
+type Outcome = byte
+type Move = int
+
+const (
+	win  Outcome = 'Z'
+	lose Outcome = 'X'
+	draw Outcome = 'Y'
+)
+const (
+	rock     Move = 1
+	paper    Move = 2
+	scissors Move = 3
+)
+
 func processInput(filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -25,7 +39,60 @@ func processInput(filename string) {
 	}
 	defer file.Close()
 
+	total_score := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		set := scanner.Text()
+		if len(set) < 3 {
+			panic("Invalid command set\n")
+		}
+		opponent_move := set[0]
+		result := set[2]
+
+		total_score = total_score + processMoves(result, byteToMove(opponent_move))
 	}
+
+	fmt.Println(total_score)
+}
+
+func byteToMove(move byte) Move {
+	if move == 'X' || move == 'A' {
+		return rock
+	}
+	if move == 'Y' || move == 'B' {
+		return paper
+	}
+	if move == 'Z' || move == 'C' {
+		return scissors
+	}
+	return 0
+}
+
+func processMoves(result Outcome, opponent Move) int {
+	if result == draw {
+		return opponent + 3
+	}
+
+	if result == lose {
+		switch opponent {
+		case rock:
+			return scissors
+		case paper:
+			return rock
+		case scissors:
+			return paper
+		}
+	}
+
+	switch opponent {
+	case rock:
+		return paper + 6
+	case paper:
+		return scissors + 6
+	case scissors:
+		return rock + 6
+	}
+
+	return 0
 }
