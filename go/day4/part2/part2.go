994e6477e7738bf256f99a554fb7ddaa785d7ed8
nrr <nickrrau@gmail.com>
2022-12-07 02:53:30 -0500

    Day 4 Part 2 solution in Go
    Pretty simple change on the same logic.
    
    Sample:
    AoC Day 4 Part 1
    845

---
+++ go/day4/part2/part2.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("AoC Day 4 Part 1")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Need to provide input")
+		return
+	}
+
+	input := os.Args[1]
+	processInput(input)
+}
+
+func processInput(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	overlaps := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		ranges := strings.Split(scanner.Text(), ",")
+		if isOverlaping(strings.Split(ranges[0], "-"), strings.Split(ranges[1], "-")) {
+			overlaps++
+		}
+	}
+
+	fmt.Println(overlaps)
+}
+
+func isOverlaping(range1 []string, range2 []string) bool {
+	beg1, _ := strconv.ParseInt(range1[0], 10, 64)
+	beg2, _ := strconv.ParseInt(range2[0], 10, 64)
+
+	end1, _ := strconv.ParseInt(range1[1], 10, 64)
+	end2, _ := strconv.ParseInt(range2[1], 10, 64)
+
+	if beg1 == beg2 && end1 == end2 {
+		return true
+	}
+
+	if beg1 > end2 || beg2 > end1 {
+		return false
+	}
+	if end1 < beg2 || end2 < beg1 {
+		return false
+	}
+
+	return true
+}
