98c7cfdef21f471c491dab29159618a0f4860fa0
nrr <nickrrau@gmail.com>
2022-12-06 20:55:05 -0500

    Day 2 Part 1 solution in Go
    Spent too much time trying to be clever, could probably do something better here but
    creating human readable names makes for much more readable logic.
    
    Example:
    AoC Day 2 Part 1
    15
    Sample:
    AoC Day 2 Part 1
    13924

--- go/day2/part1.go
+++ go/day2/part1.go
@@ -18,6 +18,14 @@ func main() {
 	processInput(input)
 }
 
+type Move = int
+
+const (
+	rock     Move = 1
+	paper    Move = 2
+	scissors Move = 3
+)
+
 func processInput(filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -25,7 +33,44 @@ func processInput(filename string) {
 	}
 	defer file.Close()
 
+	total_score := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		set := scanner.Text()
+		if len(set) < 3 {
+			panic("Invalid command set\n")
+		}
+		opponent_move := set[0]
+		player_move := set[2]
+
+		total_score = total_score + processMoves(byteToMove(player_move), byteToMove(opponent_move))
 	}
+
+	fmt.Println(total_score)
+}
+
+func byteToMove(move byte) Move {
+	if move == 'X' || move == 'A' {
+		return rock
+	}
+	if move == 'Y' || move == 'B' {
+		return paper
+	}
+	if move == 'Z' || move == 'C' {
+		return scissors
+	}
+	return 0
+}
+
+func processMoves(player Move, opponent Move) int {
+	if player == opponent {
+		return player + 3
+	}
+
+	if (player == rock && opponent == paper) || (player == paper && opponent == scissors) || (player == scissors && opponent == rock) {
+		return player
+	}
+
+	return player + 6
 }
